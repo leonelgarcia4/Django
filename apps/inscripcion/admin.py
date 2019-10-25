c386c780a310b3d17534cb20200b3e2f0f46f93b
leonelgarcia4 <36853509+leonelgarcia4@users.noreply.github.com>
2019-10-25 10:33:11 -0500

    segundo commit de prueba

--- apps/inscripcion/admin.py
+++ apps/inscripcion/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Usuario, Horario, dias
-# Register your models here.
+# Register your models he
 
 admin.site.register(Usuario)
 admin.site.register(Horario)
